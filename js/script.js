71fa2c46839cea444a8cc9bd6ea75ce60733faae
MandalorCreator <90126701+Vargur31rus@users.noreply.github.com>
2023-09-19 10:53:23 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -86,3 +86,9 @@ if (menuLinks.length > 0) {
        }
 }
 }
+
+
+setInterval(function(){
+    var a = document.getElementById('blink').style.opacity || 1;
+    document.getElementById('blink').style.opacity = ((parseInt(a))?0:1);
+},1e3);
